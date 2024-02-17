c904016674b64f6ea19960ccb5da41a0d72617eb
Tirso Samalungo <greytearsdev@gmail.com>
2024-02-17 18:37:28 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const mode =
+  process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+module.exports = {
+  mode: mode,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  devtool: 'source-map',
+  devServer: {
+    static: './dist',
+  },
+};
