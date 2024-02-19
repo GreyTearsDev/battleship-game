c499976a02b3838485985eb86f72fac9412ab5d0
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-19 09:22:24 +0100

    feat: write tests for the createShip factory function

---
+++ tests/ship.test.js
@@ -0,0 +1,47 @@
+const createShip = require('../src/modules/ship');
+
+describe('Ship Methods', () => {
+  let ship;
+
+  beforeEach(() => {
+    ship = createShip();
+  });
+
+  describe('Length Related Methods', () => {
+    test('ship should have a setLength method', () => {
+      expect(ship.setLength).toBeDefined();
+    });
+
+    test('ship should have a getLength method', () => {
+      expect(ship.getLength).toBeDefined();
+    });
+    
+    test('setLength and getLength should work correctly', () => {
+      ship.setLength(5)
+      expect(ship.getLength()).toBe(5);
+    });
+  });
+
+  describe('Damage Related Methods', () => {
+    test('ship should have a hitCount method', () => {
+      expect(ship.hitCount).toBeDefined();
+    });
+    
+    test('ship should have a hit method', () => {
+      expect(ship.hit).toBeDefined();
+    });
+    
+    test('hit method shouuld increase hitCount', () => {
+      ship.hit()
+      let damage = ship.hitCount()
+      expect(damage).toBe(1);
+    });
+    
+    test('ship should be sunk when hit enough times', () => {
+      ship.setLength(2);
+      ship.hit();
+      ship.hit();
+      expect(ship.isSunk()).toBeTruthy();
+    });
+  });
+});
