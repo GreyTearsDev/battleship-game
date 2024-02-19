8afc7d2fb7ac80b37e2ad4a842815054ddfaf666
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-19 19:48:45 +0100

    fix: remove tests for setLength and setName

---
+++ tests/board.test.js
@@ -0,0 +1,14 @@
+const board = require('../src/modules/board')
+
+describe('Board Methods', () {
+  let gameboard;
+
+  beforeEach(() => {
+    gameboard = board()
+  })
+
+  test('game')
+
+
+  
+})
