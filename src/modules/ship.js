c02292b04f70558e4345a5a17cc2a0934cba1574
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-19 09:22:24 +0100

    docs: document the createShip factory functio

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,22 +1,51 @@
-'use strict'
+'use strict';
 
+/**
+ * Creates a ship object with methods to manage its properties and state.
+ * @returns {Object} A ship object with methods for setting length, getting length,
+ *                   recording hits, counting hits, and determining if the ship is sunk.
+ */
 let createShip = () => {
   let ship = [];
   let damage = 0;
-  
-  const hitCount = () => damage;
-  const hit = () => ++damage;      
-  const setLength = (len) => ship.length = len;
+
+  /**
+   * Sets the length of the ship.
+   * @param {number} len - The length of the ship.
+   */
+  const setLength = (len) => (ship.length = len);
+
+  /**
+   * Retrieves the length of the ship.
+   * @returns {number} The length of the ship.
+   */
   const getLength = () => ship.length;
-  const isSunk = () => hitCount() >= getLength() ? true : false;
-  
+
+  /**
+   * Retrieves the count of hits on the ship.
+   * @returns {number} The count of hits on the ship.
+   */
+  const hitCount = () => damage;
+
+  /**
+   * Records a hit on the ship.
+   */
+  const hit = () => ++damage;
+
+  /**
+   * Checks if the ship is sunk.
+   * @returns {boolean} True if the number of hits equals or exceeds the ship's length, otherwise false.
+   */
+  const isSunk = () => (hitCount() >= getLength() ? true : false);
+
+  // expose public mthods
   return {
     setLength,
     getLength,
     hit,
     hitCount,
     isSunk,
-  }
-}
+  };
+};
 
 module.exports = createShip;
