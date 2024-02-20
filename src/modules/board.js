978e42522900394bfc2456c68c7b55dcd563728e
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-20 10:50:25 +0100

    fix: condition check in placeShip method
    now it correctly checks the orientation of the ship and places it in the correct coordinates

--- src/modules/board.js
+++ src/modules/board.js
@@ -1,5 +1,35 @@
+import Ship from "./ship";
+
 function GameBoard() {
+  let board = [
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0], 
+    [0,0,0,0,0,0,0,0,0,0],
+  ];
+
+  const placeShip = (ship, col, row) => {
+    for (let i = 0; i < ship.length; i++) {
+      if (ship.orientation() === "horizontal") {
+        board[row][col++] = ship.length;
+      } else {
+        board[row++][col] = ship.lenght;
+      }
+    }
+  }
+
+  const getBoard = () => board;
   
+  return {
+    placeShip,
+    getBoard
+  }
 }
 
 module.exports = GameBoard;
