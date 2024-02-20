c27b2b2c92124f258d6e8768e175cf7676d0a295
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-20 19:23:56 +0100

    feat: add test for isLegal method

--- tests/board.test.js
+++ tests/board.test.js
@@ -2,26 +2,34 @@ const Gameboard = require('../src/modules/board')
 
 describe('Gameboard Methods', () => {
   let gameboard;
+  let ship;
+  const coor = [8, 5]; // coordinates
+  
 
   beforeEach(() => {    
     gameboard = Gameboard()
-  })
-
-  test('placeShip should be able to place ships at specific coordinates', () => {
-    const ship = {
+    ship = {
       name: "Destroyer",
-      length: 5, 
+      length: 4, 
       orientation() {
         return "horizontal" 
       }
     }
-    const board = gameboard.getBoard()
-    gameboard.placeShip(ship, 2, 2);
-    expect(board[2][2]).toBe(ship.length)
+  })
+
+  test('placeShip should be able to place ships at specific coordinates', () => {
+    const board = gameboard.getBoard();
+    gameboard.placeShip(ship, coor[0], coor[1]);
+    expect(board[coor[0]][coor[1]]).toBe(ship.length);
   });
 
   test("isLegal should say if a ship can be placed at a specific coordinate", () => {
+    expect(gameboard.isLegal(ship, coor[0], coor[1])).toBeTruthy();
     
+    coor[0] = 5;
+    coor[1] = 7;
+    
+    expect(gameboard.isLegal(ship, coor[0], coor[1])).toBeFalsy();
   })
 
 
