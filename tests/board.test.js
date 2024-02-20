374cb1a3d25b2efebd172988f37f9d76881e93dd
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-20 18:28:33 +0100

    feat: add test for placeShip method

--- tests/board.test.js
+++ tests/board.test.js
@@ -1,13 +1,28 @@
-const board = require('../src/modules/board')
+const Gameboard = require('../src/modules/board')
 
-describe('Board Methods', () {
+describe('Gameboard Methods', () => {
   let gameboard;
 
-  beforeEach(() => {
-    gameboard = board()
+  beforeEach(() => {    
+    gameboard = Gameboard()
   })
 
-  test('game')
+  test('placeShip should be able to place ships at specific coordinates', () => {
+    const ship = {
+      name: "Destroyer",
+      length: 5, 
+      orientation() {
+        return "horizontal" 
+      }
+    }
+    const board = gameboard.getBoard()
+    gameboard.placeShip(ship, 2, 2);
+    expect(board[2][2]).toBe(ship.length)
+  });
+
+  test("isLegal should say if a ship can be placed at a specific coordinate", () => {
+    
+  })
 
 
   
