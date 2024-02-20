3ad8439ad9e9bd9f4637cf8b1dcfa39be9083118
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-20 19:29:20 +0100

    doc: document methods in Gameboard factory function

--- src/modules/board.js
+++ src/modules/board.js
@@ -12,6 +12,12 @@ function GameBoard() {
     [0,0,0,0,0,0,0,0,0,0],
   ];
 
+/**
+ * Places a ship on the game board at the specified row and column.
+ * @param {Object} ship - The ship object to be placed on the board.
+ * @param {number} row - The row index where the ship will be placed.
+ * @param {number} col - The column index where the ship will be placed.
+ */
   const placeShip = (ship, row, col) => {
     for (let i = 0; i < ship.length; i++) {
       if (ship.orientation() === "horizontal") {
@@ -22,8 +28,19 @@ function GameBoard() {
     }
   }
 
+/**
+ * Retrieves the current state of the game board.
+ * @returns {Array} The current game board.
+ */
   const getBoard = () => board;
 
+/**
+ * Checks if placing a ship at the specified row and column is legal.
+ * @param {Object} ship - The ship object to be placed on the board.
+ * @param {number} row - The row index to check for legality.
+ * @param {number} col - The column index to check for legality.
+ * @returns {boolean} True if placing the ship is legal, otherwise false.
+ */
   const isLegal = (ship, row, col) => {
     if (ship.orientation() === "horizontal") {
       return ship.length + col < board[row].length;
