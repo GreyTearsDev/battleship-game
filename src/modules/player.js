fde483c7850b501e6c4ef05d27a5915359059f6c
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-22 18:49:06 +0100

    doc: add documentation to the Player factory function

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,11 +1,28 @@
 'use strict'
  import gameboard from '../modules/board';
 
+/**
+ * Represents a player in the game.
+ * @constructor
+ * @returns {Object} An object representing a player.
+ */
 export function Player() {
+/**
+   * The game board of the player.
+   * @type {Object}
+   */
  const BOARD = gameboard();
+
+ /**
+   * Attacks the enemy player's game board at the specified row and column.
+   * @param {Object} enemyPlayer - The enemy player object.
+   * @param {number} row - The row index of the attack.
+   * @param {number} col - The column index of the attack.
+   */
  const attack = (enemyPlayer, row, col) => {
    enemyPlayer.BOARD.receiveAttack(row, col)
  };
+
  return {
   BOARD,
   attack,
