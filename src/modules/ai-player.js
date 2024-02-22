e000dd42d200749aa463896618d0b00859adb551
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-22 19:24:10 +0100

    feat: implement attack method

--- src/modules/ai-player.js
+++ src/modules/ai-player.js
@@ -4,7 +4,8 @@ import getRandomInt from '../utilities/random-int'
 
 export function AIPlayer() {
   const player = Player();
-
+  const BOARD = player.BOARD;
+  
   const getAttackCoordinates = () => {
     let coordinates = [];
     coordinates.push(getRandomInt(10));
@@ -12,4 +13,20 @@ export function AIPlayer() {
     return coordinates;
   } 
   
+  const attack = (enemyPlayer) => {
+    let coordinates = [...getAttackCoordinates()];
+
+    if (!player.usedCoordinates.has(coordinates)) {
+      let row, col = [...coordinates];
+      player.attack(enemyPlayer, row, col);
+      player.usedCoordinates.add(coordinates);
+      return true;
+    }
+    return false;
+  }
+
+  return {
+    BOARD,
+    attack,
+  }
 }
