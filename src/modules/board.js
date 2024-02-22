e9ce92114f7589366b1a8fa4288bf6ed6818d18c
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-22 13:20:07 +0100

    feat: implement sllShipsSunk() method

--- src/modules/board.js
+++ src/modules/board.js
@@ -23,12 +23,12 @@ function GameBoard() {
     destroyer: Ship("destroyer", 2, false),
   }
 
-/**
- * Places a ship on the game board at the specified row and column.
- * @param {Object} ship - The ship object to be placed on the board.
- * @param {number} row - The row index where the ship will be placed.
- * @param {number} col - The column index where the ship will be placed.
- */
+  /**
+   * Places a ship on the game board at the specified row and column.
+   * @param {Object} ship - The ship object to be placed on the board.
+   * @param {number} row - The row index where the ship will be placed.
+   * @param {number} col - The column index where the ship will be placed.
+   */
   const placeShip = (ship, row, col) => {
     for (let i = 0; i < ship.length; i++) {
       if (ship.orientation() === "horizontal") {
@@ -39,19 +39,19 @@ function GameBoard() {
     }
   }
 
-/**
- * Retrieves the current state of the game board.
- * @returns {Array} The current game board.
- */
+  /**
+   * Retrieves the current state of the game board.
+   * @returns {Array} The current game board.
+   */
   const getBoard = () => board;
 
-/**
- * Checks if placing a ship at the specified row and column is legal.
- * @param {Object} ship - The ship object to be placed on the board.
- * @param {number} row - The row index to check for legality.
- * @param {number} col - The column index to check for legality.
- * @returns {boolean} True if placing the ship is legal, otherwise false.
- */
+  /**
+   * Checks if placing a ship at the specified row and column is legal.
+   * @param {Object} ship - The ship object to be placed on the board.
+   * @param {number} row - The row index to check for legality.
+   * @param {number} col - The column index to check for legality.
+   * @returns {boolean} True if placing the ship is legal, otherwise false.
+   */
   const isLegal = (ship, row, col) => {
     if (ship.orientation() === "horizontal") {
       return ship.length + col < board[row].length;
@@ -60,6 +60,12 @@ function GameBoard() {
     }
   }
   
+  /**
+   * Handles receiving an attack on the game board.
+   * @param {number} row - The row index of the attack.
+   * @param {number} col - The column index of the attack.
+   * @returns {boolean} - True if the attack hits a ship, false otherwise.
+   */
   const receiveAttack = (row, col) => {
     if (board[row][col] === 0) {
       missedShots.add([row,col].toString());
@@ -75,7 +81,22 @@ function GameBoard() {
     return true
   }
 
+  /**
+   * Retrieves the set of missed shots on the game board.
+   * @returns {Set} - The set containing the coordinates of missed shots.
+   */
   const getMissedShots = () => missedShots;
+
+    /**
+   * Checks if all ships on the game board are sunk.
+   * @returns {boolean} - True if all ships are sunk, false otherwise.
+   */
+  const allShipsSunk = () => {
+    for (let ship in ships) {
+      if (!ship.isSunk()) return false;
+    }
+    return true;
+  }
   
   return {
     placeShip,
@@ -83,6 +104,7 @@ function GameBoard() {
     isLegal,
     receiveAttack, 
     getMissedShots,
+    allShipsSunk,
   }
 }
 
