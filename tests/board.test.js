503134ab00ea1b47800222c69f0f2221e2de2db0
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-22 12:18:02 +0100

    feat: add tests for receiveAttack() method

--- tests/board.test.js
+++ tests/board.test.js
@@ -1,20 +1,28 @@
-const Gameboard = require('../src/modules/board')
+import Gameboard from '../src/modules/board';
+import Ship from '../src/modules/ship';
+import {jest} from '@jest/globals'
+
+jest.mock('../src/modules/ship')
 
 describe('Gameboard Methods', () => {
   let gameboard;
   let ship;
   const coor = [8, 5]; // coordinates
   
-
   beforeEach(() => {    
-    gameboard = Gameboard()
+    gameboard = Gameboard();
+  
     ship = {
-      name: "Destroyer",
+      name: "destroyer",
       length: 4, 
-      orientation() {
-        return "horizontal" 
-      }
-    }
+      damage: 0,
+      orientation: jest.fn(() => "horizontal"),
+      hit: jest.fn(() => ++ship.damage),
+      hitCount: jest.fn(() => ship.damage),
+      getLength: jest.fn(() => ship.length)
+    };
+    Ship.mockReturnValue(ship)
+  
   })
 
   test('placeShip should be able to place ships at specific coordinates', () => {
@@ -30,6 +38,29 @@ describe('Gameboard Methods', () => {
     coor[1] = 7;
     
     expect(gameboard.isLegal(ship, coor[0], coor[1])).toBeFalsy();
+  });
+
+  describe("receiveAttack Related Functionality", () => {
+
+    beforeEach(() => {
+      gameboard.placeShip(ship, coor[0], coor[1]);
+    })
+    
+    test("should determine if the attack hit a ship or not", () => {
+      expect(gameboard.receiveAttack(coor[0], coor[1])).toBeTruthy()
+      expect(gameboard.receiveAttack(coor[0] - 1, coor[1])).toBeFalsy()
+    });
+
+    test("should send the 'hit' function to the correct ship", () => {
+      gameboard.receiveAttack(coor[0], coor[1]);
+      expect(ship.hitCount()).toBe(ship.damage);
+    });
+
+    test("should record the coordinates of missed shots", () => {
+      gameboard.receiveAttack(coor[0] - 3, coor[1]);
+      const missedShots = gameboard.getMissedShots();
+      expect(missedShots.size).toBeGreaterThan(0)
+    })
   })
 
 
