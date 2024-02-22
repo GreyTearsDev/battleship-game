a366d4eeb814c16e42a5fe0812dd6f7757952f11
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-22 18:59:38 +0100

    feat: allow players to keep track of used coordinates

--- src/modules/player.js
+++ src/modules/player.js
@@ -7,24 +7,25 @@
  * @returns {Object} An object representing a player.
  */
 export function Player() {
-/**
+  /**
    * The game board of the player.
    * @type {Object}
    */
- const BOARD = gameboard();
-
- /**
+  const BOARD = gameboard();
+  const usedCoordinates = new Set();
+   /**
    * Attacks the enemy player's game board at the specified row and column.
    * @param {Object} enemyPlayer - The enemy player object.
    * @param {number} row - The row index of the attack.
    * @param {number} col - The column index of the attack.
    */
- const attack = (enemyPlayer, row, col) => {
-   enemyPlayer.BOARD.receiveAttack(row, col)
- };
+   const attack = (enemyPlayer, row, col) => {
+     enemyPlayer.BOARD.receiveAttack(row, col)
+   };
 
- return {
-  BOARD,
-  attack,
- }
+   return {
+    BOARD,
+    usedCoordinates,
+    attack,
+   }
 }
