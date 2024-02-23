39b475f78bfa6ca9656c9e979c071f5be58147c1
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-23 12:05:04 +0100

    doc: document the AIPlayer factory function

--- src/modules/ai-player.js
+++ src/modules/ai-player.js
@@ -1,9 +1,22 @@
 'use strict'
 import getRandomInt from '../utilities/random-int'
 
+/**
+ * Represents an AI player in the game.
+ * @param {Object} player - The player object representing the AI player.
+ * @returns {Object} An object representing an AI player.
+ */
 export function AIPlayer(player) {
+  /**
+   * The game board of the AI player.
+   * @type {Object}
+   */
   const gameboard = player.gameboard;
   
+  /**
+   * Generates random attack coordinates.
+   * @returns {Array} An array containing the row and column indices of the attack.
+   */
   const getAttackCoordinates = () => {
     let coordinates = [];
     coordinates.push(getRandomInt(10));
@@ -11,6 +24,11 @@ export function AIPlayer(player) {
     return coordinates;
   } 
   
+   /**
+   * Attacks the enemy player's game board with random coordinates.
+   * @param {Object} enemyPlayer - The enemy player object.
+   * @returns {boolean} True if the attack was successful, false if the coordinates were already used.
+   */
   const attack = (enemyPlayer) => {
     let coordinates = getAttackCoordinates();
 
