b901a81ebd70e28b885fdbc0153be06c47a79bcf
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-23 21:45:21 +0100

    fix: write new webpack.config files
    now it is possible to run the page with live reloading without errors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './styles.css';
+import { Player } from './modules/player';
+import { AIPlayer } from './modules/ai-player';
+
+console.log('olºaaaaaaa');
+(function () {
+  const player = Player();
+  const computer = AIPlayer(Player());
+  console.log(player);
+  console.log(computer);
+})();
