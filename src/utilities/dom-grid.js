be6e96ae086d522d63966f44f9fa985fa99e1ea7
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-25 17:11:37 +0100

    fix: Add the 'playerID' parameter to the 'getGridCell' function for more precise element retrieval within grid structures
    improves the flexibility and usability of the function, making it easierto target specific grid cells associated with a particular player

--- src/utilities/dom-grid.js
+++ src/utilities/dom-grid.js
@@ -3,12 +3,13 @@
 
 /**
  * Retrieves a DOM element within a grid-like structure based on specified row and column attributes.
+ * @param {string|number} playerID - The ID of the player associated with the grid.
  * @param {string|number} row - The value of the `data-row` attribute of the desired grid cell.
  * @param {string|number} col - The value of the `data-col` attribute of the desired grid cell.
  * @returns {Element|null} The DOM element that matches the specified row and column attributes within the grid structure, or `null` if no matching element is found.
 */
-export function getGridCell(row, col) {
-  return document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
+export function getGridCell(playerID, row, col) {
+  return document.querySelector(`[data-playerID="${playerID}"][data-row="${row}"][data-col="${col}"]`);
 }
 
 /**
