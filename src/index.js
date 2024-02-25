f5b73bd385fdf73958369d3068b1de41483f4272
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-25 08:04:08 +0100

    feat: create the ships

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import './styles.css';
 import { Player } from './modules/player';
 import { AIPlayer } from './modules/ai-player';
+import Ship from './modules/ship'
 
-console.log('olºaaaaaaa');
+
 (function () {
+  const ships = {
+    carrier: Ship('Carrier', 5),
+    battleship: Ship('Battleship', 4),
+    cruiser: Ship('Cruiser', 3),
+    destroyer: Ship('Destroyer', 2)
+  }
   const player = Player();
   const computer = AIPlayer(Player());
   console.log(player);
   console.log(computer);
+  console.log(ships);
 })();
+
