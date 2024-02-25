d5f3ae38eb75412f8d01232621955795a2b309ec
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-25 16:19:28 +0100

    feat: implement getShipDOMElements utility function
    gets the ships' coordinates from a player's gameboard, iterates over them while using the getGridCell function to return the dom elements whose data attributes match the coordinates

--- src/utilities/get-grid-cell.js
+++ src/utilities/get-grid-cell.js
@@ -7,6 +7,18 @@
  * @param {string|number} col - The value of the `data-col` attribute of the desired grid cell.
  * @returns {Element|null} The DOM element that matches the specified row and column attributes within the grid structure, or `null` if no matching element is found.
 */
-export default function getGridCell(row, col) {
+export function getGridCell(row, col) {
   return document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
+}
+
+export function getShipDOMElements(player) {
+  let shipDOMElements = []
+  const shipsCoordinates = player.gameboard.shipCoordinates;
+
+  for (let coordinate of shipsCoordinates) {
+    let [row, col] = coordinate.split(',');
+    shipDOMElements.push(getGridCell(row, col));
+  }
+
+  return shipDOMElements;
 }
