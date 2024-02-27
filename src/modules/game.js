189a2dd9592d4f67b4ad887868795120b5f64d5f
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-27 20:03:36 +0100

    feat: implement 'getTheWinner' funciton in index.js
    it checks if the gameboard of either player has had all of its ships sunk, which would indicate that the player in question has lost. It then returns the player who still has active ships on its gameboard

---
+++ src/modules/game.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/**
+  * Checks if there is a winner by looking at the boards of each player and
+  * checking if all of its ships have been sunk
+  * @param {object} player - The player object containing ships information.
+  * @param {object} computer - The computer object containing ships information.
+  * @return {object} Returns the object whose all ships in its gameboard have been 
+  *                  sunk, or null if all both of them has at least one ship in their
+  *                  gameboards.
+ */
+export function getTheWinner(player, computer) {
+  if (player.gameboard.allShipsSunk()) return player;
+  if (computer.gameboard.allShipsSunk()) return computer;
+  return null;
+}
+
+
+//TODO:display winner
+
+
