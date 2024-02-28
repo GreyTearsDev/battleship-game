b347aa2b2d06f041827867ab163dbdb277c839a1
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-28 10:19:41 +0100

    fix: Refactor attack method in Player module
    the method now checks for illegal attacks by verifying if the coordinates have been previously used. If the attack is legal, it proceeds to attack the enemy player's game board and returns the result as either hit or miss. This refactor enhances the robustness and clarity of the attack mechanism.

--- src/modules/player.js
+++ src/modules/player.js
@@ -8,16 +8,11 @@
  * @returns {Object} An object representing a player.
  */
 export function Player(playerName) {
-  /**
-   * The game board of the player.
-   * @type {Object}
-   */
   const gameboard = Gameboard();
   const name = playerName;
-
   const usedCoordinates = new Set();
 
-   /**
+  /**
    * Attacks the enemy player's game board at the specified row and column.
    * @param {Object} enemyPlayer - The enemy player object.
    * @param {number} row - The row index of the attack.
@@ -25,7 +20,12 @@ export function Player(playerName) {
    * @return {boolean} True if the attack is successful, false otherwise.
    */
    const attack = (enemyPlayer, row, col) => {
-     return enemyPlayer.gameboard.receiveAttack(row, col);
+     const coordinates = `${row},${col}`;
+     if (usedCoordinates.has(coordinates)) return "illegal";
+     const attackResult = enemyPlayer.gameboard.receiveAttack(row, col);
+
+     usedCoordinates.add(coordinates);
+     return attackResult ? "hit" : "miss";
    };
 
    /**
