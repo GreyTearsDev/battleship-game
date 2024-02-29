fd84cd4ccfc17bb48d389e912930e076af347108
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-29 05:45:59 +0100

    refactor: Refactor renderShipsOnBoard function
    it now takes playerID and playerObject as parameters instead of separate player and computer objects. The function retrieves DOM elements representing ships for the specified playerID, applies the ship CSS class to each element, and renders them on the board.

--- src/modules/dom/render-ships.js
+++ src/modules/dom/render-ships.js
@@ -2,17 +2,13 @@
 import { setClass, getShipDOMElements } from "../../utilities/dom";
 
 /**
- * Renders the ships on the game board for both the player and the computer.
+ * Rends the ships on the game board for both the player and the computer.
  * This function retrieves DOM elements representing the ships for each player,
  * applies the "ship" CSS class to each element, and renders them on the board.
  * @param {object} player - The player object containing ship information.
  * @param {object} computer - The computer object containing ship information.
  */
-export function renderShipsOnBoard(player, computer) {
-  // Render the ships on the board
-  let playerDOMElements = getShipDOMElements("player", player);
-  let computerDOMElements = getShipDOMElements("computer", computer);
-  
-  playerDOMElements.forEach((cell) => setClass(cell, "ship"))
-  computerDOMElements.forEach((cell) => setClass(cell, "ship"))
+export function renderShipsOnBoard(playerID, playerObject) {
+  let DOMElements = getShipDOMElements(playerID, playerObject);
+  DOMElements.forEach((cell) => setClass(cell, "ship"))
 }
