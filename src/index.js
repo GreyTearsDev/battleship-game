3a7e3cbaf4afade8e7f29315018949e012991ff5
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-29 07:06:39 +0100

    refactor: Refactor import for Ship module to use createShips function for both players
    import statement in the main game module now utilizes the createShips function for both player and computer, centralizing the ship creation logic. It also simplifies the initialization process for player and computer ships

--- src/index.js
+++ src/index.js
@@ -1,52 +1,49 @@
 import './styles.css';
 import { Player } from './modules/player';
 import { AIPlayer } from './modules/ai-player';
-import Ship from './modules/ship'
+import { createShips }  from './modules/ship'
 import { getAllDOMGameboardCells, getGridCell, renderAttack } from './utilities/dom'
 import { renderShipsOnBoard } from './modules/dom/render-ships';
 import { getTheWinner } from './modules/game'
 
 
-  const ships = {
-    carrier: new Ship('Carrier', 5),
-    battleship: new Ship('Battleship', 4),
-    cruiser: new Ship('Cruiser', 3),
-    destroyer: new Ship('Destroyer', 2)
-  };
-  const player = new Player("Human Player");
-  const computer = AIPlayer(new Player("Computer"));
+const player = new Player("Human Player");
+const playerShips = new createShips();
+const computer = AIPlayer(new Player("Computer"));
+const computerShips = new createShips();
 
-  let row = 0;
-  let col = 0;
+let row = 0;
+let col = 0;
+
+// Place the ships on the board
+for (let ship in playerShips) {
+  player.gameboard.placeShip(playerShips[ship], row++, col++);
+}
+computer.placeShipRandomly(computerShips)
+renderShipsOnBoard("player", player);
+
+let computerGridCells = getAllDOMGameboardCells("computer");  
+
+computerGridCells.forEach((cell) => {
+  cell.addEventListener("click", function(){
+    let row = cell.dataset.row;
+    let col = cell.dataset.col;  
+    const attackResult = player.attack(computer, row, col);
+
+    renderAttack(cell, attackResult);
   
-  // Place the ships on the board
-  for (let ship in ships) {
-    ship = ships[ship]
-    player.gameboard.placeShip(ship, row++, col++);
-  }
-  computer.placeShipRandomly(ships)
-  renderShipsOnBoard(player, computer);
+    if (attackResult === "illegal") return;
+ 
+    computer.attack(player).then((result) => {
+      const [attackResult, attackedRow, attackedCol] = result;
+      const attackedCell = getGridCell("player", attackedRow, attackedCol);
+      renderAttack(attackedCell, attackResult);
+  });
 
-  let computerGridCells = getAllDOMGameboardCells("computer");  
-
-  computerGridCells.forEach((cell) => {
-    cell.addEventListener("click", function(){
-      let row = cell.dataset.row;
-      let col = cell.dataset.col;
-      
-      const attackResult = player.attack(computer, row, col);
-      renderAttack(cell, attackResult);
-
-      computer.attack(player).then((result) => {
-        const [attackResult, attackedRow, attackedCol] = result;
-        const attackedCell = getGridCell("player", attackedRow, attackedCol);
-        renderAttack(attackedCell, attackResult);
-    });
-
-                
-    const WINNER = getTheWinner(player, computer);
-    if (WINNER !== null) {
-      console.log(WINNER.getName(), "won")
-    } 
-    });
-  })
+              
+  const WINNER = getTheWinner(player, computer);
+  if (WINNER !== null) {
+    console.log(WINNER.getName(), "won")
+  } 
+  });
+})
