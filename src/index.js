d0a8f2396a4f22d4f8d7e3684df9f6679b9d54ab
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-03-05 05:17:38 +0100

    refactor: Remove commented-out code and unused imports from index.js
    - removed commented-out code related to player instantiation, ship placement, rendering, and event handling from index.js.
    - removed unused imports related to player, AIPlayer, createShips, getAllDOMGameboardCells, and renderShips from index.js.
    - updated index.js to import and call the initializeGame function from game.js and display the start screen using displayStartScreen.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,5 @@
 import './styles.css';
-import { Player } from './modules/player';
-import { AIPlayer } from './modules/ai-player';
-import { createShips }  from './modules/ship'
-import { getAllDOMGameboardCells } from './utilities/dom'
-import { renderShips } from './modules/dom/render-ships';
-import { getTheWinner, handleAttack } from './modules/game'
+import { initializeGame } from './modules/game'
+import { displayStartScreen } from './utilities/dom';
 
-const player = new Player("Human Player");
-const playerShips = new createShips();
-const computer = AIPlayer(new Player("Computer"));
-const computerShips = new createShips();
-const computerGridCells = getAllDOMGameboardCells("computer");  
-
-
-player.placeShipRandomly(playerShips)
-computer.placeShipRandomly(computerShips);
-renderShips("player", player);
-
-computerGridCells.forEach((cell) => cell.addEventListener("click", attackHandler));
-
-function attackHandler(event) {
-  const cell = event.target;
-  handleAttack(cell, player, computer)
-  let winner = getTheWinner(player, computer);
-  if (winner) console.log(winner.getName())
-}
+displayStartScreen(initializeGame);
